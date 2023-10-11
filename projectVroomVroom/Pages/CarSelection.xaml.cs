c445ca2d645ab93d810daac76909e2c59627db8d
Jelmer <jelmerdejong@kpnmail.nl>
2023-10-11 22:34:47 +0200

    Added car selection screen

--- projectVroomVroom/Pages/CarSelection.xaml.cs
+++ projectVroomVroom/Pages/CarSelection.xaml.cs
@@ -20,9 +20,61 @@ namespace projectVroomVroom.Pages
     /// </summary>
     public partial class CarSelection : UserControl
     {
+
+        private bool _isReady1 = false; // Ready state for both players
+        private bool _isReady2 = false;
+
         public CarSelection()
         {
             InitializeComponent();
+        }
+
+        private void RemovePlaceholder(object sender, RoutedEventArgs e)
+        {
+            var textBox = sender as TextBox; // Get the textbox that is clicked
+            if (textBox.Text == "Naam") // If the textbox contains the placeholder text
+            {
+                textBox.Text = ""; // Remove the placeholder text
+            }
+        }
+
+        private void AddPlaceholder(object sender, RoutedEventArgs e)
+        {
+            var textBox = sender as TextBox; // Get the textbox that is clicked
+            if (string.IsNullOrWhiteSpace(textBox.Text)) // If the textbox is empty
+            {
+                textBox.Text = "Naam"; // Add the placeholder text
+            }
+        }
+
+        private void ReadyButton1Click(object sender, RoutedEventArgs e)
+        {
+            _isReady1 = !_isReady1; // Toggle the ready state
+
+            CheckIfBothPlayersReady();
+
+            ReadyButton1.Background = _isReady1 ? 
+                new SolidColorBrush(Color.FromRgb(35, 225, 32)) : 
+                new SolidColorBrush(Color.FromRgb(177, 177, 177)); // Change the background color of the button to show if player is ready
+        }
+
+        private void ReadyButton2Click(object sender, RoutedEventArgs e)
+        {
+            _isReady2 = !_isReady2; // Toggle the ready state
+
+            CheckIfBothPlayersReady();
+
+            ReadyButton2.Background = _isReady2 ? 
+                new SolidColorBrush(Color.FromRgb(35, 225, 32)) : 
+                new SolidColorBrush(Color.FromRgb(177, 177, 177)); // Change the background color of the button to show if player is ready
+        }
+
+        private void CheckIfBothPlayersReady()
+        {
+            if (_isReady1 && _isReady2) // If both players are ready
+            {
+                this.Content = new Circuit.Circuit(); // Navigate to the Circuit page
+            }
         }
     }
 }
