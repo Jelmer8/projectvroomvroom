0aa91b3a1fb02d2646dd3415c115a116e7b93768
RedArmyNL <yoeri.zondervan@gmail.com>
2023-10-18 14:20:37 +0200

    SCRAPPED
    scap the segments map doesnt work.

--- projectVroomVroom/MainWindow.xaml.cs
+++ projectVroomVroom/MainWindow.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using projectVroomVroom.Circuit;
-using projectVroomVroom.TrackSegments;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -41,7 +40,6 @@ namespace projectVroomVroom
         double frictionOnHeavyGrass = 0.85;
         int[,] trackArray = new int[5, 5];
         double lastTrackAngle = 0;
-        ITrackSegment lastSegment = null;
         DateTime startTime;
         DateTime currentTime;
         ICircuit circuit = new Circuit.Circuit();
@@ -52,13 +50,12 @@ namespace projectVroomVroom
         Path trackCenterLine;
         Path mapWhiteLine;
         Path mapBlackLine;
-        List<TrackLineSegment> trackLineList = new List<TrackLineSegment>();
         PolyLineSegment polyLineSegment;
         const double MAX_TRACK_FRICTION_RANGE = 110.0;
         const double MAX_LIGHTGRASS_FRICTION_RANGE = 120.0;
         const double MAX_LIGHTGRASS_SPEED_REDUCTION = 1.5;
         const double MAX_HEAVYGRASS_SPEED_REDUCTION = 1.5;
-       /// List<Kart> kartList = new List<Kart>();
+        /// List<Kart> kartList = new List<Kart>();
         double circuitLength = 0.0;
         Point trackStartPoint;
         const int TOTAL_LAPS = 5;
@@ -72,7 +69,6 @@ namespace projectVroomVroom
         public MainWindow()
         {
             InitializeComponent();
-         ///   InitializeCars();
             InitializeTrack();
 
         }
@@ -125,62 +121,6 @@ namespace projectVroomVroom
                 HorizontalAlignment = System.Windows.HorizontalAlignment.Left,
                 VerticalAlignment = System.Windows.VerticalAlignment.Top
             };
-
-            var drawingBrush = new DrawingBrush()
-            {
-                Stretch = Stretch.None,
-                TileMode = TileMode.Tile,
-                Viewport = new Rect(0, 0, 0.1, 0.1)
-            };
-
-            mapWhiteLine = new Path()
-            {
-                Stroke = new SolidColorBrush(Color.FromRgb(0xFF, 0xFF, 0x00)),
-                StrokeThickness = 8,
-                Margin = new Thickness(-11, -11, 0, 0),
-                Opacity = 0.5
-            };
-
-            mapBlackLine = new Path()
-            {
-                Stroke = new SolidColorBrush(Color.FromRgb(0x00, 0x00, 0x00)),
-                StrokeThickness = 8,
-                Margin = new Thickness(-10, -10, 0, 0),
-                Opacity = 0.0
-            };
-
-            var strPoints = new StringBuilder();
-            var strMapPoints = new StringBuilder();
-            strPoints.AppendFormat("M {0},{1}", trackStartPoint.X * 8 + 200, trackStartPoint.Y * 8 + 200);
-            strMapPoints.AppendFormat("M {0},{1}", trackStartPoint.X, trackStartPoint.Y);
-            max_track_point_X = trackStartPoint.X;
-            max_track_point_Y = trackStartPoint.Y;
-            int pointCount = 0;
-            var lastPoint = trackStartPoint;
-            var index = 0;
-            foreach (var point in polyLineSegment.Points)
-            {
-                if (point.X > max_track_point_X)
-                    max_track_point_X = point.X;
-
-                if (point.Y > max_track_point_Y)
-                    max_track_point_Y = point.Y;
-
-                var trackLineSegment = new TrackLineSegment(index, circuitLength, new Point(lastPoint.X * 8, lastPoint.Y * 8), new Point(point.X * 8, point.Y * 8));
-                trackLineList.Add(trackLineSegment);
-
-                index++;
-
-                trackLineSegment.Milestone = circuitLength;
-                circuitLength += trackLineSegment.Length;
-
-                index++;
-
-
-
-
-
-
-            }
-         }
+        }
     }
+}
