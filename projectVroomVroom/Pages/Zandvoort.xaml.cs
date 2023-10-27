411619c1c65921f0a7b474f9ff8b4785e25311d6
RedArmyNL <yoeri.zondervan@gmail.com>
2023-10-27 14:42:34 +0200

    car update

--- projectVroomVroom/Pages/Zandvoort.xaml.cs
+++ projectVroomVroom/Pages/Zandvoort.xaml.cs
@@ -154,5 +154,64 @@ namespace projectVroomVroom.Pages
             MenuLabel.Visibility = Visibility.Hidden;
             VisibleCheck = false;
         }
+
+        /// <summary>
+        ///     Hieronder volgt de code voor auto, even gescheiden want het moet wss naar boven verplaatsd worden
+        /// </summary>
+        /// 
+
+
+
+        private double playerOneCarPositionX = 0;
+        private double playerOneCarPositionY = 0;
+
+
+        private void MainWindow_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.W:
+                    MoveUpPlayerOneCar();
+                    break;
+                case Key.A:
+                    MoveLeftPlayerOneCar();
+                    break;
+                case Key.S:
+                    MoveDownPlayerOneCar();
+                    break;
+                case Key.D:
+                    MoveRightPlayerOneCar();
+                    break;
+            }
+        }
+
+        private void MoveUpPlayerOneCar()
+        {
+            playerOneCarPositionY -= 10;
+            Player1.Margin = new Thickness(playerOneCarPositionX, playerOneCarPositionY, 0, 0);
+        }
+
+        private void MoveLeftPlayerOneCar()
+        {
+            playerOneCarPositionX -= 10;
+            Player1.Margin = new Thickness(playerOneCarPositionX, playerOneCarPositionY, 0, 0);
+        }
+
+        private void MoveDownPlayerOneCar()
+        {
+            playerOneCarPositionY += 10;
+            Player1.Margin = new Thickness(playerOneCarPositionX, playerOneCarPositionY, 0, 0);
+        }
+
+        private void MoveRightPlayerOneCar()
+        {
+            playerOneCarPositionX += 10;
+            Player1.Margin = new Thickness(playerOneCarPositionX, playerOneCarPositionY, 0, 0);
+        }
+    }
+}
+
+
+
     }
 }
