16bcd7bd235e7bb6a8e2203ac0f0527c96aebb27
RedArmyNL <yoeri.zondervan@gmail.com>
2023-10-28 15:34:34 +0200

    workable driveable car
    vroom vroom

--- projectVroomVroom/Pages/Zandvoort.xaml.cs
+++ projectVroomVroom/Pages/Zandvoort.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using projectVroomVroom;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.ConstrainedExecution;
@@ -13,14 +14,23 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace projectVroomVroom.Pages
 {
-    /// <summary>
-    /// Interaction logic for Zandvoort.xaml
-    /// </summary>
+
     public partial class Zandvoort : UserControl
     {
+        private bool isTurningLeft = false;
+        private bool isTurningRight = false;
+        private double carRotationAngle = 0;
+        private double carX = 100; 
+        private double carY = 100; 
+        private double carVelocityForward = 0; 
+        private double carVelocityBackward = 0; 
+        private bool isAccelerating = false;
+        private bool isReversing = false;
+
 
         private MainWindow mainWindow = (MainWindow)Application.Current.MainWindow;
         private Boolean VisibleCheck = false;
@@ -31,188 +41,120 @@ namespace projectVroomVroom.Pages
         public Zandvoort()
         {
             InitializeComponent();
-            MenuCanvas.Focus();
+            canvas.Focus();
+            this.KeyDown += OnKeyDown2;
+            this.KeyUp += OnKeyUp;
+            DispatcherTimer gameLoopTimer = new DispatcherTimer();
+            gameLoopTimer.Interval = TimeSpan.FromMilliseconds(16); 
+            gameLoopTimer.Tick += GameLoop;
+            gameLoopTimer.Start();
         }
 
-        private void OnKeyDown(object sender, KeyEventArgs e)
+    
+
+        private void OnKeyDown2(object sender, KeyEventArgs e)
         {
-            if (e.Key == Key.Escape)
+            if (e.Key == Key.Left)
             {
-                if (!VisibleCheck)
-                {
-                    Menu.Visibility = Visibility.Visible;
-                    ButtonMenuGoMain.Visibility = Visibility.Visible;
-                    ButtonMenuOptions.Visibility = Visibility.Visible;
-                    ButtonMenuResume.Visibility = Visibility.Visible;
-                    MenuImage.Visibility = Visibility.Visible;
-                    MenuLabel.Visibility = Visibility.Visible;
-                    ButtonOptionsBack.Visibility = Visibility.Hidden;
-                    VisibleCheck = true;
-                }
-                else
-                {
-                    Menu.Visibility = Visibility.Hidden;
-                    ButtonMenuGoMain.Visibility = Visibility.Hidden;
-                    ButtonMenuOptions.Visibility = Visibility.Hidden;
-                    ButtonMenuResume.Visibility = Visibility.Hidden;
-                    MenuImage.Visibility = Visibility.Hidden;
-                    MenuLabel.Visibility = Visibility.Hidden;
-                    ButtonOptionsBack.Visibility = Visibility.Hidden;
-                    CheckboxSoundToggle.Visibility = Visibility.Hidden;
-                    CheckboxMusictoggle.Visibility = Visibility.Hidden;
-                    SoundImage.Visibility = Visibility.Hidden;
-                    MuteSoundImage.Visibility = Visibility.Hidden;
-                    MusicImage.Visibility = Visibility.Hidden;
-                    MuteMusicImage.Visibility = Visibility.Hidden;
-                    VisibleCheck = false;
-                }
+                isTurningLeft = true;
+            }
+            else if (e.Key == Key.Right)
+            {
+                isTurningRight = true;
+            }
+            else if (e.Key == Key.Up)
+            {
+                isAccelerating = true;
+            }
+            else if (e.Key == Key.Down)
+            {
+                isReversing = true;
+            }
+
+        }
+        private void OnKeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Left)
+            {
+                isTurningLeft = false;
+            }
+            else if (e.Key == Key.Right)
+            {
+                isTurningRight = false;
+            }
+            else if (e.Key == Key.Up)
+            {
+                isAccelerating = false;
+            }
+            else if (e.Key == Key.Down)
+            {
+                isReversing = false;
             }
         }
-        private void MainMenuButton(object sender, RoutedEventArgs e)
+
+        private void GameLoop(object sender, EventArgs e)
         {
-            mainWindow.Content = new Pages.MainMenu();
-        }
-        private void MenuOptionsButton(object sender, RoutedEventArgs e)
-        {
-            ButtonMenuGoMain.Visibility = Visibility.Hidden;
-            ButtonMenuOptions.Visibility = Visibility.Hidden;
-            ButtonMenuResume.Visibility = Visibility.Hidden;
-            ButtonOptionsBack.Visibility = Visibility.Visible;
-            CheckboxSoundToggle.Visibility = Visibility.Visible;
-            CheckboxMusictoggle.Visibility = Visibility.Visible;
-            if (SoundMuted)
+
+            
+            if (isTurningLeft)
             {
-                SoundImage.Visibility = Visibility.Hidden;
-                MuteSoundImage.Visibility = Visibility.Visible;
+                carRotationAngle -= 5; 
+            }
+            if (isTurningRight)
+            {
+                carRotationAngle += 5; 
+            }
+
+            
+            double carAcceleration = 0.1; 
+            if (isAccelerating)
+            {
+                carVelocityForward += carAcceleration;
+            }
+            else if (isReversing)
+            {
+                carVelocityBackward += carAcceleration;
             }
             else
             {
-                SoundImage.Visibility = Visibility.Visible;
-                MuteSoundImage.Visibility = Visibility.Hidden;
+                
+                carVelocityForward *= 0.95;
+                carVelocityBackward *= 0.95;
             }
 
-            if (MusicMuted)
-            {
-                MusicImage.Visibility = Visibility.Hidden;
-                MuteMusicImage.Visibility = Visibility.Visible;
-            }
-            else
-            {
-                MusicImage.Visibility = Visibility.Visible;
-                MuteMusicImage.Visibility = Visibility.Hidden;
-            }
-        }
+            
+            double maxVelocity = 5.0; 
+            carVelocityForward = Math.Min(maxVelocity, carVelocityForward);
+            carVelocityBackward = Math.Min(maxVelocity, carVelocityBackward);
 
-        private void OptionsBackButton(object sender, RoutedEventArgs e)
-        {
-            Menu.Visibility = Visibility.Visible;
-            ButtonMenuGoMain.Visibility = Visibility.Visible;
-            ButtonMenuOptions.Visibility = Visibility.Visible;
-            ButtonMenuResume.Visibility = Visibility.Visible;
-            ButtonOptionsBack.Visibility = Visibility.Hidden;
-            CheckboxSoundToggle.Visibility = Visibility.Hidden;
-            CheckboxMusictoggle.Visibility = Visibility.Hidden;
-            SoundImage.Visibility = Visibility.Hidden;
-            MuteSoundImage.Visibility = Visibility.Hidden;
-            MusicImage.Visibility = Visibility.Hidden;
-            MuteMusicImage.Visibility = Visibility.Hidden;
-        }
+            
+            double totalVelocity = carVelocityForward - carVelocityBackward;
 
-        private void SoundCheckboxHandleChecked(object sender, RoutedEventArgs e)
-        {
-            SoundImage.Visibility = Visibility.Hidden;
-            MuteSoundImage.Visibility = Visibility.Visible;
-            SoundMuted = true;
-        }
+            
+            double carRotationRadians = carRotationAngle * Math.PI / 180;
 
-        private void SoundCheckboxHandleUnchecked(object sender, RoutedEventArgs e)
-        {
-            SoundImage.Visibility = Visibility.Visible;
-            MuteSoundImage.Visibility = Visibility.Hidden;
-            SoundMuted = false;
-        }
+            
+            carX += totalVelocity * Math.Cos(carRotationRadians);
+            carY += totalVelocity * Math.Sin(carRotationRadians);
 
-        private void MusicCheckboxHandleChecked(object sender, RoutedEventArgs e)
-        {
-            MusicImage.Visibility = Visibility.Hidden;
-            MuteMusicImage.Visibility = Visibility.Visible;
-            MusicMuted = true;
-        }
+            
+            Canvas.SetLeft(Car, carX);
+            Canvas.SetTop(Car, carY);
 
-        private void MusicCheckboxHandleUnchecked(object sender, RoutedEventArgs e)
-        {
-            MusicImage.Visibility = Visibility.Visible;
-            MuteMusicImage.Visibility = Visibility.Hidden;
-            MusicMuted = false;
-        }
-
-        private void MenuResumeButton(object sender, RoutedEventArgs e)
-        {
-            Menu.Visibility = Visibility.Hidden;
-            ButtonMenuGoMain.Visibility = Visibility.Hidden;
-            ButtonMenuOptions.Visibility = Visibility.Hidden;
-            ButtonMenuResume.Visibility = Visibility.Hidden;
-            MenuImage.Visibility = Visibility.Hidden;
-            MenuLabel.Visibility = Visibility.Hidden;
-            VisibleCheck = false;
-        }
-
-        /// <summary>
-        ///     Hieronder volgt de code voor auto, even gescheiden want het moet wss naar boven verplaatsd worden
-        /// </summary>
-        /// 
-
-
-        public double Acceleration = 0;
-        public double Player1Acceleration = 0;
-        public double Player1Speed = 0;
-        public double frictionOnTrack = 1.0;
-        public double groundFactor = 1.0;
-
-
-
-
-
-
-
-
-        void gameLoopTimer_Tick(object sender, KeyEventArgs e) {
-
-
-
-
-            if (e.Key == Key.W)
-            {
-                Player1Acceleration += 0.001;
-            }
-            else if (e.Key == Key.S)
-            {
-
-                Player1Acceleration -= 0.01;
-                Player1Speed *= (1.0 - frictionOnTrack);
-            }
-            else if (e.Key == Key.A)
-            {
-               ///  car.Speed *= (1.0 - frictionOnTrack);
-            }
-            else if (e.Key == Key.D)
-            {
-
-            }
-            else 
-            {
-                Player1Speed *= (1 - frictionOnTrack);
-            }
-
-
-
-            if (Player1Speed < 10)
-
-
-                Player1Speed = Player1Speed + Player1Acceleration * groundFactor;
-
-
+            
+            ((RotateTransform)Car.RenderTransform).Angle = carRotationAngle;
         }
     }
- }
+
+
+
+
+
+    }
+
+
+
+
+
+
 
