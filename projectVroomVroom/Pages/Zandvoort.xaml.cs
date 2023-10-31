834efdbc6e79aa0e337d6ee0883ded74150e562a
jearus342 <jemyr.sprott@student.nhlstenden.com>
2023-10-31 11:14:10 +0100

    Tweede auto werkende gekregen.

--- projectVroomVroom/Pages/Zandvoort.xaml.cs
+++ projectVroomVroom/Pages/Zandvoort.xaml.cs
@@ -27,13 +27,30 @@ namespace projectVroomVroom.Pages
     {
         private bool isTurningLeft = false;
         private bool isTurningRight = false;
+
+        private bool isTurningLeft2 = false;
+        private bool isTurningRight2 = false;
+
         private double carRotationAngle = 0;
         private double carX = 100; 
-        private double carY = 100; 
+        private double carY = 100;
+
+        private double carRotationAngle2 = 0;
+        private double carX2 = 100;
+        private double carY2 = 100;
+
         private double carVelocityForward = 0; 
-        private double carVelocityBackward = 0; 
+        private double carVelocityBackward = 0;
+        private double carVelocityForward2 = 0;
+        private double carVelocityBackward2 = 0;
+
         private bool isAccelerating = false;
         private bool isReversing = false;
+
+        private bool isAccelerating2 = false;
+        private bool isReversing2 = false;
+
+
 
         private MediaPlayer mediaPlayer;
         private MainWindow mainWindow = (MainWindow)Application.Current.MainWindow;
@@ -97,6 +114,24 @@ namespace projectVroomVroom.Pages
                 isReversing = true;
             }
 
+
+            else if (e.Key == Key.A)
+            {
+                isTurningLeft2 = true;
+            }
+            else if (e.Key == Key.D)
+            {
+                isTurningRight2 = true;
+            }
+            else if (e.Key == Key.W)
+            {
+                isAccelerating2 = true;
+            }
+            else if (e.Key == Key.S)
+            {
+                isReversing2 = true;
+            }
+
         }
         private void OnKeyUp(object sender, KeyEventArgs e)
         {
@@ -116,6 +151,24 @@ namespace projectVroomVroom.Pages
             {
                 isReversing = false;
             }
+
+
+            else if (e.Key == Key.A)
+            {
+                isTurningLeft2 = false;
+            }
+            else if (e.Key == Key.D)
+            {
+                isTurningRight2 = false;
+            }
+            else if (e.Key == Key.W)
+            {
+                isAccelerating2 = false;
+            }
+            else if (e.Key == Key.S)
+            {
+                isReversing2 = false;
+            }
         }
 
         private void GameLoop(object sender, EventArgs e)
@@ -131,8 +184,21 @@ namespace projectVroomVroom.Pages
                 carRotationAngle += 5; 
             }
 
-            
+
+
+            if (isTurningLeft2)
+            {
+                carRotationAngle2 -= 5;
+            }
+            if (isTurningRight2)
+            {
+                carRotationAngle2 += 5;
+            }
+
+
             double carAcceleration = 0.1; 
+             double carAcceleration2 = 0.1;
+
             if (isAccelerating)
             {
                 carVelocityForward += carAcceleration;
@@ -143,23 +209,48 @@ namespace projectVroomVroom.Pages
             }
             else
             {
-                
-                carVelocityForward *= 0.95;
+            
+
+
+                    carVelocityForward *= 0.95;
                 carVelocityBackward *= 0.95;
             }
 
-            
-            double maxVelocity = 5.0; 
+
+            if (isAccelerating2)
+            {
+                carVelocityForward2 += carAcceleration2;
+            }
+            else if (isReversing2)
+            {
+                carVelocityBackward2 += carAcceleration2;
+            }
+            else
+            {
+
+                carVelocityForward2 *= 0.95;
+                carVelocityBackward2 *= 0.95;
+
+            }
+                double maxVelocity = 5.0; 
             carVelocityForward = Math.Min(maxVelocity, carVelocityForward);
             carVelocityBackward = Math.Min(maxVelocity, carVelocityBackward);
 
-            
+
+            double maxVelocity2 = 5.0;
+            carVelocityForward2 = Math.Min(maxVelocity2, carVelocityForward2);
+            carVelocityBackward2 = Math.Min(maxVelocity2, carVelocityBackward2);
+
             double totalVelocity = carVelocityForward - carVelocityBackward;
 
-            
+            double totalVelocity2 = carVelocityForward2 - carVelocityBackward2;
+
+
             double carRotationRadians = carRotationAngle * Math.PI / 180;
 
-            
+            double carRotationRadians2 = carRotationAngle2 * Math.PI / 180;
+
+
             carX += totalVelocity * Math.Cos(carRotationRadians);
             carY += totalVelocity * Math.Sin(carRotationRadians);
 
@@ -167,8 +258,18 @@ namespace projectVroomVroom.Pages
             Canvas.SetLeft(Car, carX);
             Canvas.SetTop(Car, carY);
 
-            
+
+            carX2 += totalVelocity2 * Math.Cos(carRotationRadians2);
+            carY2 += totalVelocity2 * Math.Sin(carRotationRadians2);
+
+
+            Canvas.SetLeft(Car2, carX2);
+            Canvas.SetTop(Car2, carY2);
+
+
             ((RotateTransform)Car.RenderTransform).Angle = carRotationAngle;
+
+            ((RotateTransform)Car2.RenderTransform).Angle = carRotationAngle2;
         }
     }
 
